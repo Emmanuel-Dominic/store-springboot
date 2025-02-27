bb04ef5d95b6676283e57130014a4dced4b9dd8a
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2025-02-27 12:58:43 +0300

    add dynamic value in HTML page

--- src/main/java/com/ematembu/storespringboot/HomeController.java
+++ src/main/java/com/ematembu/storespringboot/HomeController.java
@@ -1,13 +1,19 @@
 package com.ematembu.storespringboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("appName", appName);
+        return "index";
     }
 }
