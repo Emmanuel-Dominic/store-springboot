371fd64f4c9965a8cb305ced5dd2c28d9ca5aa44
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2025-02-27 11:30:10 +0300

    initialize project

---
+++ src/test/java/com/ematembu/storespringboot/StoreSpringbootApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ematembu.storespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
