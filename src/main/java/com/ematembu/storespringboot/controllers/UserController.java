e1ec6ad672cec813acfa40d3b2bb53b9e7d41dbe
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2025-03-03 23:13:55 +0300

    adds a new user

--- src/main/java/com/ematembu/storespringboot/controllers/UserController.java
+++ src/main/java/com/ematembu/storespringboot/controllers/UserController.java
@@ -5,9 +5,7 @@ import com.ematembu.storespringboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,7 +17,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("/users")
+    @RequestMapping(value = "/users", method= RequestMethod.GET)
     public ResponseEntity<?> findAllUsers() {
         List<User> users =  userService.getUsers();
         if(users.isEmpty()){
@@ -42,6 +40,18 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }else{
             return ResponseEntity.status(HttpStatus.OK).body(user);
+        }
+    }
+
+    @RequestMapping(value = "/users", method= RequestMethod.POST)
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        try{
+            User newUser = userService.registerUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        }catch (Exception e){
+            Map<String, String> response = new HashMap<>();
+            response.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 }
--- src/main/java/com/ematembu/storespringboot/services/UserService.java
+++ src/main/java/com/ematembu/storespringboot/services/UserService.java
@@ -2,17 +2,19 @@ package com.ematembu.storespringboot.services;
 
 import com.ematembu.storespringboot.models.User;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    List<User> users = Arrays.asList(
+    List<User> users = new ArrayList<>(Arrays.asList(
                 new User(1, "Manuel", 30, "ematembu2@gmail.com", "Manuel123", "+256700701616", "Lungujja"),
                 new User(2, "Dominic", 29, "ematembu@gmail.com", "Dominic123", "+256702701616", "Lungujja"),
                 new User(3, "Timothy", 32, "timothy21@gmail.com", "Timothy123", "+256788084708", "Lungujja")
-            );
+            ));
 
     public List<User> getUsers() {
         return users;
@@ -22,5 +24,10 @@ public class UserService {
         return users.stream()
                 .filter(user -> user.getId() == id)
                 .findFirst().orElse(null);
+    }
+
+    public User registerUser(User user) {
+        users.add(user);
+        return user;
     }
 }
